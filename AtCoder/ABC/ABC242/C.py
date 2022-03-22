8cf820296a8d24d63360d461a39d5dc0f6a3fa8e
valusun <chobicoco0327@gmail.com>
2022-03-22 21:59:09 +0900

    Add ABC242 A-C

---
+++ AtCoder/ABC/ABC242/C.py
@@ -0,0 +1,12 @@
+N = int(input())
+dp = [[0]*11 for _ in range(N+1)]
+for i in range(1, 10):
+    dp[1][i] = 1
+
+MOD = 998244353
+
+for i in range(1, N):
+    for j in range(1, 10):
+        dp[i+1][j] = (dp[i][j-1]+dp[i][j]+dp[i][j+1])%MOD
+
+print(sum(dp[N])%MOD)
