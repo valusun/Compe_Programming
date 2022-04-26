fb6edd5e05ceda39178cd69b4660a406a6ff3271
valusun <chobicoco0327@gmail.com>
2022-04-26 22:00:27 +0900

    [update]体裁修正/命名修正

--- AtCoder/ABC/ABC200/D.py
+++ AtCoder/ABC/ABC200/D.py
@@ -1,21 +1,29 @@
-N = int(input())
-A = list(map(int,input().split()))
+def OutputSequenceWithLength(sequence):
+    print(len(sequence), *sequence)
 
-Mod = [[] for _ in range(200)]
 
-for i in range(1, 2**N):
-    selected_idx = []
-    selected_num = 0
-    for j in range(N):
-        if i&(1<<j):
-            selected_num = (selected_num + A[j])%200
-            selected_idx.append(j+1)
-    if len(Mod[selected_num]):
-        print("Yes")
-        print(len(Mod[selected_num]), *Mod[selected_num])
-        print(len(selected_idx), *selected_idx)
-        exit()
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    MOD = 200
+    selected_sum_mod200_indexes = [[] for _ in range(200)]
+    for i in range(1, 2**N):
+        selected_sum = 0
+        selected_indexes = []
+        for j in range(N):
+            if i & (1 << j):
+                selected_sum = (selected_sum + A[j]) % MOD
+                selected_indexes.append(j + 1)
+        if selected_sum_mod200_indexes[selected_sum]:
+            print("Yes")
+            OutputSequenceWithLength(selected_indexes)
+            OutputSequenceWithLength(*selected_sum_mod200_indexes[selected_sum])
+            break
+        else:
+            selected_sum_mod200_indexes[selected_sum].append(selected_indexes)
     else:
-        Mod[selected_num] = selected_idx
+        print("No")
 
-print("No")
+
+if __name__ == "__main__":
+    main()
