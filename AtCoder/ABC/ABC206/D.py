6b87d7ff77b54cdb91f801acdb6f0e8ab0c11a1a
valusun <chobicoco0327@gmail.com>
2022-05-06 21:49:00 +0900

    [add]

---
+++ AtCoder/ABC/ABC206/D.py
@@ -0,0 +1,65 @@
+class UnionFind:
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    # Returns the root of the tree to which x belongs
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    # Union x-tree and y-tree
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    # Returns x-tree number of elements in the tree
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    # Are x and y the same tree?
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    # Returns the element of the tree to which x belongs
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    # Returns a list of all root elements
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    # Returns Group Count
+    def group_count(self):
+        return len(self.roots())
+
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    uf = UnionFind(3 * 10**5)
+    for i in range(N // 2):
+        if A[i] != A[-(i + 1)]:
+            uf.union(A[i], A[-(i + 1)])
+    ans = 0
+    for i in set(A):
+        if i != uf.find(i):
+            ans += 1
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
