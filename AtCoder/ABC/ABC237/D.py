e13318a4b6cb6be16abcb47e31e2f4da1c485be3
valusun <chobicoco0327@gmail.com>
2022-06-18 19:21:14 +0900

    [update]不要importの削除

--- AtCoder/ABC/ABC237/D.py
+++ AtCoder/ABC/ABC237/D.py
@@ -1,11 +1,14 @@
-N = int(input())
-S = input()
+def main():
+    N = int(input())
+    S = input()
+    L, R = [], []
+    for i, s in enumerate(S):
+        if s == "L":
+            R.append(i)
+        else:
+            L.append(i)
+    print(*(L + [N] + R[::-1]))
 
-L,R = [],[]
-for i in range(N):
-    if S[i] == 'L':
-        R.append(i)
-    else:
-        L.append(i)
 
-print(*L,N,*R[::-1])
+if __name__ == "__main__":
+    main()
