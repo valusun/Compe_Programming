a87cbf4eb98634bcd5a8a2e36e0f46d767532e7c
valusun <chobicoco0327@gmail.com>
2022-07-17 23:42:28 +0900

    [update]体裁修正

--- AtCoder/ABC/ABC059/C.py
+++ AtCoder/ABC/ABC059/C.py
@@ -1,53 +1,42 @@
-from typing import Tuple
-
-
 def main():
     N = int(input())
     A = list(map(int, input().split()))
 
-    def ActionCusumPositive(now_sum: int, action_cnt: int, idx: int) -> Tuple[int, int]:
-        """累積和を正にするときの動作を行い、操作後の合計値と現在までの操作回数を返す"""
-        if now_sum + A[idx] <= 0:
-            action_cnt += 1 - (now_sum + A[idx])
-            now_sum = 1
-        else:
-            now_sum += A[idx]
-        return (now_sum, action_cnt)
-
-    def ActionCusumNegative(now_sum: int, action_cnt: int, idx: int) -> Tuple[int, int]:
-        """累積和を負にするときの動作を行い、操作後の合計値と現在までの操作回数を返す"""
-        if now_sum + A[idx] >= 0:
-            action_cnt += 1 + (now_sum + A[idx])
-            now_sum = -1
-        else:
-            now_sum += A[idx]
-        return (now_sum, action_cnt)
-
+    even_positive_cnt = 0
     now_sum = 0
-    first_positive_cnt = 0
+
     for i in range(N):
         if i % 2 == 0:
-            now_sum, first_positive_cnt = ActionCusumPositive(
-                now_sum, first_positive_cnt, i
-            )
+            if now_sum + A[i] <= 0:
+                even_positive_cnt += 1 - (now_sum + A[i])
+                now_sum = 1
+            else:
+                now_sum += A[i]
         else:
-            now_sum, first_positive_cnt = ActionCusumNegative(
-                now_sum, first_positive_cnt, i
-            )
+            if now_sum + A[i] >= 0:
+                even_positive_cnt += 1 + (now_sum + A[i])
+                now_sum = -1
+            else:
+                now_sum += A[i]
 
+    even_negative_cnt = 0
     now_sum = 0
-    first_negative_cnt = 0
+
     for i in range(N):
         if i % 2 == 0:
-            now_sum, first_negative_cnt = ActionCusumNegative(
-                now_sum, first_negative_cnt, i
-            )
+            if now_sum + A[i] >= 0:
+                even_negative_cnt += 1 + (now_sum + A[i])
+                now_sum = -1
+            else:
+                now_sum += A[i]
         else:
-            now_sum, first_negative_cnt = ActionCusumPositive(
-                now_sum, first_negative_cnt, i
-            )
+            if now_sum + A[i] <= 0:
+                even_negative_cnt += 1 - (now_sum + A[i])
+                now_sum = 1
+            else:
+                now_sum += A[i]
 
-    print(min(first_positive_cnt, first_negative_cnt))
+    print(min(even_positive_cnt, even_negative_cnt))
 
 
 if __name__ == "__main__":
