860483f2b53588e7fa0ec56c78916440e3cfbb02
valusun <chobicoco0327@gmail.com>
2022-09-12 21:09:47 +0900

    [add]

---
+++ AtCoder/ABC/ABC103/A.py
@@ -0,0 +1,7 @@
+def main():
+    A = list(map(int, input().split()))
+    print(max(A) - min(A))
+
+
+if __name__ == "__main__":
+    main()
---
+++ AtCoder/ABC/ABC103/C.py
@@ -0,0 +1,8 @@
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    print(sum(A) - N)
+
+
+if __name__ == "__main__":
+    main()
