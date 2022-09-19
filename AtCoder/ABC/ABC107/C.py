0d68133ead4ffad5e0fc2ccd9e793b826a56f356
valusun <chobicoco0327@gmail.com>
2022-09-19 10:57:30 +0900

    [update]読みやすくした

--- AtCoder/ABC/ABC107/C.py
+++ AtCoder/ABC/ABC107/C.py
@@ -1,14 +1,24 @@
 def main():
-    N, K = map(int, input().split())
+    _, K = map(int, input().split())
     A = list(map(int, input().split()))
-    ans = float("inf")
 
-    for i in range(N - K + 1):
-        left = A[i]
-        right = A[i + K - 1]
-        ans = min(
-            ans, min(abs(left) + abs(left - right), abs(right) + abs(right - left))
-        )
+    positive, negative = [0], [0]
+    for num in A:
+        positive.append(num) if num > 0 else negative.append(abs(num))
+    negative = sorted(negative)
+
+    positive_size = len(positive)
+    negative_size = len(negative)
+
+    ans = float("inf")
+    for p in range(positive_size):
+        n = K - p
+        if 0 <= n and n < negative_size:
+            ans = min(ans, positive[p] * 2 + negative[n])
+    for n in range(negative_size):
+        p = K - n
+        if 0 <= p and p < positive_size:
+            ans = min(ans, negative[n] * 2 + positive[p])
     print(ans)
 
 
