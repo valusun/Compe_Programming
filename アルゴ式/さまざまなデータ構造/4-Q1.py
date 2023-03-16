1c8566c20a8b6afdd9193651d9648286a5a1c854
valusun <chobicoco0327@gmail.com>
2023-03-16 21:58:12 +0900

    add

---
+++ アルゴ式/さまざまなデータ構造/4-Q1.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.nxt = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_start(self, value: str) -> None:
+        """先頭に要素を挿入する"""
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        new_node.nxt = self.head
+        self.head = new_node
+
+    def output(self, k: int) -> None:
+        """k番目から出力する"""
+        current = self.head
+        cnt = 0
+        output_data = []
+        while current is not None and cnt <= k:
+            output_data.append(current.val)
+            current = current.nxt
+            cnt += 1
+        print(*output_data)
+
+
+def main():
+    Q = int(input())
+    linked = LinkedList()
+    for _ in range(Q):
+        n, d = input().split()
+        if n == "0":
+            linked.insert_start(d)
+        else:
+            linked.output(int(d) - 1)
+
+
+if __name__ == "__main__":
+    main()
