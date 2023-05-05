aa24a464409bbe0763c45969bf86d4c6b1c2c637
valusun <chobicoco0327@gmail.com>
2023-05-05 20:47:46 +0900

    :sparkles: update 分かりやすい解法に変えた

--- AtCoder/ABC/ABC213/E.py
+++ AtCoder/ABC/ABC213/E.py
@@ -4,37 +4,33 @@ from collections import deque
 def main():
     H, W = map(int, input().split())
     field = [list(input()) for _ in range(H)]
-    INF = 10**9
-    broken_cnt = [[INF] * W for _ in range(H)]
-    normal_move_h = [0, 1, 0, -1]
-    normal_move_w = [1, 0, -1, 0]
-    broken_move_h = [-2, -2, -2, -1, -1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2]
-    broken_move_w = [-1, 0, 1, -2, -1, 0, 1, 2, -2, -1, 1, 2, -2, -1, 0, 1, 2, -1, 0, 1]
-
+    field_cnt = [[10**9] * W for _ in range(H)]
+    dh = [1, 0, -1, 0]
+    dw = [0, 1, 0, -1]
+    warp_dh = [-2, -2, -2, -1, -1, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2]
+    warp_dw = [1, 0, -1, 2, 1, 0, -1, -2, 2, 1, -1, -2, 2, 1, 0, -1, -2, 1, 0, -1]
     Q = deque()
     Q.append((0, 0, 0))
     while Q:
         h, w, d = Q.popleft()
-        if h == (H - 1) and w == (W - 1):
+        if h == H - 1 and w == W - 1:
             break
-        if field[h][w] == ".":
-            for mh, mw in zip(normal_move_h, normal_move_w):
-                nh = h + mh
-                nw = w + mw
-                is_area = 0 <= nh < H and 0 <= nw < W
-                if is_area and d < broken_cnt[nh][nw]:
-                    broken_cnt[nh][nw] = d
-                    Q.appendleft((nh, nw, d))
-        else:
-            for mh, mw in zip(broken_move_h, broken_move_w):
-                nh = h + mh
-                nw = w + mw
-                tmp_d = d + 1
-                is_area = 0 <= nh < H and 0 <= nw < W
-                if is_area and tmp_d < broken_cnt[nh][nw]:
-                    broken_cnt[nh][nw] = tmp_d
-                    Q.append((nh, nw, tmp_d))
-    print(broken_cnt[H - 1][W - 1])
+        for mh, mw in zip(dh, dw):
+            nh = h + mh
+            nw = w + mw
+            is_area = 0 <= nh < H and 0 <= nw < W
+            if is_area and field[nh][nw] == "." and d < field_cnt[nh][nw]:
+                field_cnt[nh][nw] = d
+                Q.appendleft((nh, nw, d))  # 優先的に見るため、すぐ取り出せる先頭に入れる
+        # 体力1を消費して、2x2の範囲に移動できると見なす
+        for mh, mw in zip(warp_dh, warp_dw):
+            nh = h + mh
+            nw = w + mw
+            is_area = 0 <= nh < H and 0 <= nw < W
+            if is_area and d + 1 < field_cnt[nh][nw]:
+                field_cnt[nh][nw] = d + 1
+                Q.append((nh, nw, d + 1))
+    print(field_cnt[H - 1][W - 1])
 
 
 if __name__ == "__main__":
