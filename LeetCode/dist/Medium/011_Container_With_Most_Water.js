e7eb8b2d5844a2e1299b62d165df77de8bd0dc98
valusun <chobicoco0327@gmail.com>
2023-09-22 15:36:14 +0900

    :tada: add

---
+++ LeetCode/dist/Medium/011_Container_With_Most_Water.js
@@ -0,0 +1,18 @@
+"use strict";
+function maxArea(height) {
+    const n = height.length;
+    let ans = 0, left = 0, right = n - 1;
+    while (left < right) {
+        const res = Math.min(height[left], height[right]) * (right - left);
+        ans = Math.max(ans, res);
+        if (height[left] < height[right]) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    return ans;
+}
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+console.log(maxArea([1, 1]));
