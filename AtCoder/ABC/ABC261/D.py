a080cd2ab55b8ce7cbd0d9c5f9f1d286536766f1
valusun <chobicoco0327@gmail.com>
2023-10-08 21:10:08 +0900

    :sparkles: update 直感的なコードに変更

--- AtCoder/ABC/ABC261/D.py
+++ AtCoder/ABC/ABC261/D.py
@@ -1,27 +1,23 @@
 from collections import Counter
 
-ans = 0
-
 
 def main():
     N, M = map(int, input().split())
     X = list(map(int, input().split()))
-    dic = Counter()
+    bonuses = Counter()
     for _ in range(M):
         c, y = map(int, input().split())
-        dic[c] = y
-
-    dp = [[0] * (N + 1) for _ in range(N + 1)]
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            dp[i][j] = dp[i - 1][j - 1] + X[i - 1] + dic[j]
-        for j in range(i):
-            dp[i][0] = max(dp[i][0], dp[i - 1][j])
-
-    ans = 0
-    for i in range(N + 1):
-        ans = max(ans, dp[N][i])
-    print(ans)
+        bonuses[c] = y
+    dp = [[-1] * (N + 1) for _ in range(N + 1)]
+    dp[0][0] = 0
+    for i in range(N):
+        for j in range(N):
+            if dp[i][j] == -1:
+                continue
+            dp[i + 1][0] = max(dp[i + 1][0], dp[i][j])
+            point = dp[i][j] + bonuses[j + 1] + X[i]
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], point)
+    print(max(dp[-1]))
 
 
 if __name__ == "__main__":
