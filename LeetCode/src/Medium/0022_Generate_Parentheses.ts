0dd98f3f6a659b7ea4a0311bc5e25005a062d5a4
valusun <chobicoco0327@gmail.com>
2023-10-28 13:23:11 +0900

    :tada: add

---
+++ LeetCode/src/Medium/0022_Generate_Parentheses.ts
@@ -0,0 +1,25 @@
+const dfs = (
+  mx: number,
+  pattern: string,
+  l_cnt: number,
+  r_cnt: number,
+  result: string[],
+): string[] => {
+  if (pattern.length == mx) {
+    result.push(pattern);
+    return result;
+  }
+  if (l_cnt > r_cnt) {
+    dfs(mx, pattern + ")", l_cnt, r_cnt + 1, result);
+  }
+  if (l_cnt < mx / 2) {
+    dfs(mx, pattern + "(", l_cnt + 1, r_cnt, result);
+  }
+  return result;
+};
+
+function generateParenthesis(n: number): string[] {
+  return dfs(n * 2, "(", 1, 0, []);
+}
+
+console.log(generateParenthesis(3));
